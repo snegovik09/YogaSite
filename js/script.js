6bbd1888f0a1c7761b77b7fec53c5e761a891b60
Sergio <sergio-kf@mail.ru>
2022-06-15 23:50:14 +0300

    modal commit

--- js/script.js
+++ js/script.js
@@ -34,4 +34,76 @@ window.addEventListener('DOMContentLoaded', function() {
             }
         }
     });
+
+    // Timer
+
+    let deadline = '2022-06-15';
+
+    function getTimeRemaining(endtime) {
+        let t = Date.parse(endtime) - Date.parse(new Date),
+            $seconds = Math.floor((t/1000) % 60),
+            $minutes = Math.floor((t/1000/60) % 60),
+            $hours = Math.floor(t/1000/60/60);
+
+        return {
+        'total': t,
+        'hours': $hours,
+        'minutes': $minutes,
+        'seconds': $seconds
+        };
+    }
+
+    function setClock(id, endtime) {
+        let $timer = document.getElementById(id),
+            $hours = $timer.querySelector('.hours'),
+            $minutes = $timer.querySelector('.minutes'),
+            $seconds = $timer.querySelector('.seconds'),
+            $timeInterval = setInterval(updateClock, 1000);
+
+        function updateClock() {
+            let t = getTimeRemaining(endtime);
+            
+            function zero(a)  {
+                if(a < 10) {
+                    return '0' + a;
+                } else return a;
+            }
+            $hours.textContent = zero(t.hours);
+            $minutes.textContent = zero(t.minutes);
+            $seconds.textContent = zero(t.seconds);
+            
+            if(t.total <= 0) {
+                clearInterval($timeInterval);
+                $hours.textContent = '00';
+                $minutes.textContent = '00';
+                $seconds.textContent = '00';
+            }
+            
+        }
+    }
+
+    setClock('timer', deadline);
+
+    //modal
+
+    let $more = document.querySelectorAll('.btn-overflow'),
+        $overlay = document.querySelector('.overlay'),
+        $close = document.querySelector('.popup-close');
+
+    $more.forEach(function(item) {
+        item.addEventListener('click', function() {
+        $overlay.style.display = 'block';
+        item.classList.add('more-splash');
+        document.body.style.overflow = 'hidden';
+        })
+    });
+
+    $close.addEventListener('click', function() {
+        $overlay.style.display = 'none';
+        $more.forEach(function(item) {
+            item.classList.remove('more-splash')
+        });
+        document.body.style.overflow = '';
+    });
 });
+
