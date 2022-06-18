3c51dc7e03dd5306064ccfe95972c06b4d25fd47
Sergio <sergio-kf@mail.ru>
2022-06-18 20:40:54 +0300

    commitPromiseYoga

--- js/script.js
+++ js/script.js
@@ -105,5 +105,61 @@ window.addEventListener('DOMContentLoaded', function() {
         });
         document.body.style.overflow = '';
     });
-});
 
+
+    //Form
+
+    let message = {
+        loading: 'Загрузка',
+        success: 'Спасибо! Скоро мы с вами свяжемся',
+        failure: 'Что-то пошло не так'
+    };
+
+    let $form = document.querySelector('.main-form'),
+        $input = $form.getElementsByTagName('input'),
+        $statusMessage = document.createElement('div');
+    $statusMessage.classList.add('status');
+
+    function sendForm(form) {
+        form.addEventListener('submit', function(event) {
+            event.preventDefault();
+            form.appendChild($statusMessage);
+            let $formData = new FormData(form);
+            function postData(formData) {
+                return new Promise(function(resolve, reject) {
+                let request = new XMLHttpRequest();
+                request.open('POST', '../server.php');
+                request.setRequestHeader('Content-Type', 'application/json; charst=utf-8');
+                let obj = {};
+                formData.forEach(function(value, key) {
+                    obj[key] = value;
+                });
+                let json = JSON.stringify(obj);
+                request.send(json);
+                    request.onreadystatechange = function() {
+                        if (request.readyState < 4) {
+                            resolve()
+                        } else if(request.readyState == 4 && request.status == 200) {
+                            resolve()
+                        } else {
+                            reject()
+                        }
+                    };
+                });
+            }
+
+            function clearInput() {
+                for (let i = 0; i < $input.length; i++) {
+                    $input[i].value = '';
+                }
+            }
+            
+            postData($formData)
+                .then(() => $statusMessage.innerHTML = message.loading)
+                .then(() => $statusMessage.innerHTML = message.success)
+                .catch(() => $statusMessage.innerHTML = message.failure)
+                .then(clearInput);
+        });
+    }
+    sendForm($form);
+});
